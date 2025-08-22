fb163dc508d7003606b6c4a4cf5e8b054b7dcce1
Esin Oliebol <zulal2024@gmail.com>
2025-08-22 18:40:34 +0200

    trying stuff

---
+++ members/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({},request))
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render({},request))
+
+    
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers':mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id =id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+
+
+# def deneme():
+#     context = {
+#         'points: 5'
+#     }
